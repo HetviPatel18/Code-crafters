deb3728a4bef32778cbef4bf77b53102b18b6ccb
krishacodes30 <krishaeducation2020@gmail.com>
2025-07-12 15:06:13 +0530

    Delete Desktop/FCHAT directory

--- Desktop/FCHAT/backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv');
-
-const mongoose = require('mongoose');
-dotenv.config();
-const connectdb=async()=>{
-await mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected successfully on Qonnect"))
-  .catch(err => console.error("MongoDB connection error:", err));}
-module.exports = connectdb;
--- Desktop/FCHAT/frontend/my-app/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import usereducer from './userslice'
-import { getuser } from './userslice'
-export const store = configureStore({
-  reducer: {
-    user:usereducer,
-    // getuse:getuser
-  },
-})
