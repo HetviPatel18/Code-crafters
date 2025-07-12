c4355514eb5c9a6aac85eef63a840c92a6d58db3
Richa2911 <24csd82@svitvasad.ac.in>
2025-07-12 15:31:52 +0530

    Update ProtectedRoute.jsx

--- Desktop/skill swap/client/client/src/components/ProtectedRoute.jsx
+++ Desktop/skill swap/client/client/src/components/ProtectedRoute.jsx
@@ -1,1 +1,7 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+export default function ProtectedRoute() {
+  const token = localStorage.getItem('token');
+  return token ? <Outlet /> : <Navigate to="/login" />;
+}
 
