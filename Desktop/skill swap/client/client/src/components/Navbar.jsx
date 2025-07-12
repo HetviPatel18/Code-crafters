7412501f2891b2ed5bccce75b59ac224414c332c
HetviPatel18 <hetvi0604@gmail.com>
2025-07-12 15:41:29 +0530

    Update Navbar.jsx

--- Desktop/skill swap/client/client/src/components/Navbar.jsx
+++ Desktop/skill swap/client/client/src/components/Navbar.jsx
@@ -17,6 +17,11 @@ export default function Navbar() {
         <NavLink to="/login" onClick={() => setOpen(false)}>Login</NavLink>
         <NavLink to="/send-request" onClick={() => setOpen(false)}>Send Request</NavLink>
       </div>
+      <NavLink to="/" end className="nav-link">Browse Skills</NavLink>
+<NavLink to="/profile" className="nav-link">My Profile</NavLink>
+<NavLink to="/swap-requests" className="nav-link">Swap Requests</NavLink>
+<NavLink to="/feedback" className="nav-link">Feedback</NavLink>
+<NavLink to="/admin" className="nav-link">Admin</NavLink>
     </nav>
   );
 }
